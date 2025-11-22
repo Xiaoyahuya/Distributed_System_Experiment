82532704d044ae3647aec0ad04dceea272e33c3b
Xiaoyahuya <3169914431@qq.com>
2025-11-22 16:45:23 +0800

    second

--- experiment_2/d1_interleaving.c
+++ experiment_2/d1_interleaving.c
@@ -5,27 +5,9 @@
 
 #define NUMTHREADS 4
 
-// 全局状态
-int next_turn = 0;
-pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
-
 void *threadfn(void *ptr) {
     int my_id = (int)ptr;
-    
-    pthread_mutex_lock(&lock);
-    while (next_turn != my_id) {
-        // TODO: 等待信号
-        pthread_cond_wait(&cond, &lock);
-    }
-    
-    // 临界区操作
     printf("Thread %d is executing strictly in order.\n", my_id);
-    next_turn++; // 移交控制权给下一个
-    
-    // TODO: 通知其他线程醒来检查
-    pthread_cond_broadcast(&cond);
-    pthread_mutex_unlock(&lock);
     return NULL;
 }
 
